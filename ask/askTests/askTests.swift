b7234f3db57bd6e69460ec9352efa32086f5528c
Kevin Hill <kevinhill@live.com>
2026-03-28 18:09:14 +0100

    Initial commit

---
+++ ask/askTests/askTests.swift
@@ -0,0 +1,17 @@
+//
+//  askTests.swift
+//  askTests
+//
+//  Created by Kevin Hill on 3/28/26.
+//
+
+import Testing
+@testable import ask
+
+struct askTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
